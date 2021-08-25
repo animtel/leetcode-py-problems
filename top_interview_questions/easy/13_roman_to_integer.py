30d735b307b7d14d828bdd5f9fdca6665653118e
Daniel Kovalenko <animtel@gmail.com>
2021-08-25 22:01:04 +0300

    Much clear solution of roman to int converter

---
+++ top_interview_questions/easy/13_roman_to_integer.py
@@ -0,0 +1,50 @@
+# 1. Two Sum problem: https://leetcode.com/problems/roman-to-integer/
+
+class Solution:
+    def __init__(self):
+        self.roman_to_int_pairs = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+
+    def romanToInt(self, s: str) -> int:
+        roman_str_len = len(s)
+
+        if roman_str_len < 1 or roman_str_len > 15:
+            return 0
+
+        result = 0
+        prev_int_to_diff = 0
+
+        for i in s:
+            int_i = self.roman_to_int_pairs[i]
+
+            if prev_int_to_diff < int_i:
+                result += (int_i - prev_int_to_diff - prev_int_to_diff)
+                prev_int_to_diff = int_i
+                continue
+
+            prev_int_to_diff = int_i
+            result += int_i
+
+        return result
+
+
+def check_hypothesis_for(hypothesis, given, should):
+    reversed = hypothesis.romanToInt(given)
+    assert reversed == should, f'Wrong reverse for number {given}, returned: {reversed}, should be {should}!'
+
+
+if __name__ == '__main__':
+    hypothesis = Solution()
+
+    check_hypothesis_for(hypothesis, 'III', 3)
+    check_hypothesis_for(hypothesis, 'IV', 4)
+    check_hypothesis_for(hypothesis, 'IX', 9)
+    check_hypothesis_for(hypothesis, 'LVIII', 58)
+    check_hypothesis_for(hypothesis, 'MCMXCIV', 1994)
